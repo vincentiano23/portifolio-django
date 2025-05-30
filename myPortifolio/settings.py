fc15945944fae22d292b68d8bed7822634269fa4
vincentiano23 <vincentmuthiani15@gmail.com>
2025-05-30 14:09:27 +0300

    Enhanced portifolio theme and responsiveness

--- myPortifolio/settings.py
+++ myPortifolio/settings.py
@@ -77,7 +77,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media Files
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
